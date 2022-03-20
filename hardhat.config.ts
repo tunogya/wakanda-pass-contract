30550562d966f5567be1aa9b2c3cab30a85d7526
tunogya <tunogya@qq.com>
2022-03-20 23:56:11 +0800

    init hardhat project

---
+++ hardhat.config.ts
@@ -0,0 +1,74 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@openzeppelin/hardhat-upgrades";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const mnemonic = process.env.HDWALLET_MNEMONIC;
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "ganache",
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      accounts: {
+        mnemonic: mnemonic,
+      },
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts: {
+        mnemonic: mnemonic,
+      },
+    },
+    arbitrum_rinkeby: {
+      url: process.env.ARBITRUM_RINKEBY_URL || "",
+      accounts: {
+        mnemonic: mnemonic,
+      },
+    },
+    bsct: {
+      url: process.env.BSCT_URL || "",
+      accounts: {
+        mnemonic: mnemonic,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
