614ac6a83ab766d9a3a6a6a827845c9f80922be6
tunogya <tunogya@qq.com>
2022-03-31 23:14:54 +0800

    update

--- contracts/CapAndTrade.sol
+++ contracts/CapAndTrade.sol
@@ -16,8 +16,11 @@ pragma solidity ^0.8.6;
 contract CapAndTrade is Ownable {
     /* ============ Global Variables ============ */
 
+    /// genesis cap of start year
     uint256 public immutable genesis;
+    /// start year
     uint256 public immutable start;
+    /// end year
     uint256 public immutable end;
 
     /// @notice CarbonCredit Token address
@@ -30,7 +33,10 @@ contract CapAndTrade is Ownable {
 
     /**
      * @notice Deploy CapAndTrade contract
-     * @param _token Address of the CarbonCredit.
+     * @param _token Address of the CarbonCredit
+     * @param _genesis genesis cap of start year
+     * @param _start start year
+     * @param _end end year
      */
     constructor(
         ICarbonCredit _token,
@@ -48,52 +54,74 @@ contract CapAndTrade is Ownable {
 
     /* ============ External Functions ============ */
 
+    /**
+     * @notice get the year of timestamp
+     * @param timestamp the query timestamp
+     * @return year the year of timestamp
+     */
     function yearOf(
         uint256 timestamp
     ) external virtual pure returns (uint256) {
         return _yearOf(timestamp);
     }
 
-
+    /**
+     * @notice get the cap of timestamp
+     * @param timestamp the query timestamp
+     * @return cap the cap of timestamp
+     */
     function capOf(
         uint256 timestamp
     ) external virtual view returns (uint256) {
         return _capOf(timestamp);
     }
 
-
+    /**
+     * @notice claim current year's allowance
+     */
     function claim() external virtual {
         _claim();
     }
 
     /* ============ Internal Functions ============ */
 
+    /**
+     * @notice get the year of timestamp
+     * @param timestamp the query timestamp
+     * @return year the year of timestamp
+     */
     function _yearOf(
         uint256 timestamp
     ) internal pure returns (uint256) {
         return (timestamp / 365 days) + 1970;
     }
 
+    /**
+     * @notice get the cap of timestamp
+     * @param timestamp the query timestamp
+     * @return cap the cap of timestamp
+     */
     function _capOf(
         uint256 timestamp
     ) internal view returns (uint256) {
         uint256 year = _yearOf(timestamp);
-        if (year >= end || year < start) {
-            return 0;
-        }
+        require(year >= start && year <= end, "CapAndTrade: year out of range");
 
         return genesis - (year - start) * genesis / (end - start);
     }
 
+    /**
+     * @notice claim current year's allowance
+     */
     function _claim() internal {
-        uint256 currentYear = _yearOf(block.timestamp);
-        require(currentYear <= end, "CapAndTrade: end");
+        uint256 year = _yearOf(block.timestamp);
+        require(year >= start && year <= end, "CapAndTrade: year out of range");
 
         mapping(uint256 => uint256) storage user = allowances[msg.sender];
-        require(user[currentYear] == 0, "CapAndTrade: you had claimed this year");
+        require(user[year] == 0, "CapAndTrade: you had claimed this year");
 
         uint256 cap = _capOf(block.timestamp);
-        user[currentYear] = cap;
+        user[year] = cap;
         token.mint(msg.sender, cap);
     }
 }
