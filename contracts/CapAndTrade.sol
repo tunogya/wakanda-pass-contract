45905637c1dd1c1cb8e84f00c1c4f6f20bc7881b
tunogya <tunogya@qq.com>
2022-04-08 21:44:35 +0800

    delete Ownable

--- contracts/CapAndTrade.sol
+++ contracts/CapAndTrade.sol
@@ -2,8 +2,6 @@
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "./interfaces/ICarbonCredit.sol";
 
 pragma solidity ^0.8.6;
@@ -13,7 +11,7 @@ pragma solidity ^0.8.6;
  * @author Wakanda Labs
  * @notice
  */
-contract CapAndTrade is Ownable {
+contract CapAndTrade {
     /* ============ Global Variables ============ */
 
     /// genesis cap of start year
