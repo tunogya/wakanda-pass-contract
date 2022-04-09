3cb30319f01aec6d66e701c7dc2cc581c4e5cb36
tunogya <tunogya@qq.com>
2022-04-09 20:20:49 +0800

    update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,11 +12,14 @@ const green = (text: string) => {
 async function main() {
   const signers = await ethers.getSigners();
   dim(`signer: ${signers[0].address}`);
-  const CarbonCredit = await ethers.getContractFactory("CarbonCredit");
+  const CarbonCredit = await ethers.getContractFactory("ChildMintableERC20");
+  const MumbaiChildManager = "0xb5505a6d998549090530911180f38aC5130101c6";
+  // const MainnetChildManager = "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa";
   const credit = await CarbonCredit.deploy(
     "Wakanda Carbon Credit",
     "WCO2",
-    signers[0].address
+    signers[0].address,
+    MumbaiChildManager
   );
   await credit.deployed();
   green(`CarbonCredit deployed to: ${credit.address}`);
@@ -24,24 +27,24 @@ async function main() {
     `hh verify --network ${network.name} ${credit.address} "Wakanda Carbon Credit" WCO2 ${signers[0].address}`
   );
 
-  const CapAndTrade = await ethers.getContractFactory("CapAndTrade");
-  const capAndTrade = await CapAndTrade.deploy(
-    credit.address,
-    "10000000000000000000",
-    2022,
-    2060
-  );
-
-  await capAndTrade.deployed();
-  green(`CapAndTrade deployed to: ${capAndTrade.address}`);
-  dim(
-    `hh verify --network ${network.name} ${capAndTrade.address} ${credit.address} 10000000000000000000 2022 2060`
-  );
-
-  dim(`Grant role to CapAndTrade...`);
-  const MINTER_ROLE = await credit.MINTER_ROLE();
-  await credit.grantRole(MINTER_ROLE, capAndTrade.address);
-  dim(`Grant success!`);
+  // const CapAndTrade = await ethers.getContractFactory("CapAndTrade");
+  // const capAndTrade = await CapAndTrade.deploy(
+  //   credit.address,
+  //   "10000000000000000000",
+  //   2022,
+  //   2060
+  // );
+  //
+  // await capAndTrade.deployed();
+  // green(`CapAndTrade deployed to: ${capAndTrade.address}`);
+  // dim(
+  //   `hh verify --network ${network.name} ${capAndTrade.address} ${credit.address} 10000000000000000000 2022 2060`
+  // );
+  //
+  // dim(`Grant role to CapAndTrade...`);
+  // const MINTER_ROLE = await credit.MINTER_ROLE();
+  // await credit.grantRole(MINTER_ROLE, capAndTrade.address);
+  // dim(`Grant success!`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
