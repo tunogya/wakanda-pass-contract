c7023e76649c4448f5534b3cc64158a038f7b56b
tunogya <tunogya@qq.com>
2022-07-28 11:47:10 +0800

    fixed

--- contracts/WakandaPass.sol
+++ contracts/WakandaPass.sol
@@ -148,7 +148,7 @@ IERC721Receiver
                             ),
                             '", "attributes": [{"trait_type": "Geohash", "value": "',
                             tokenURI_,
-                            '"}, {"display_type": "number", "trait_type": "Length", "value": ',
+                            '"}, {"trait_type": "Length", "value": ',
                             Strings.toString(bytes(tokenURI_).length),
                             '}], "background_color": "E5E5E5"}'
                         )
